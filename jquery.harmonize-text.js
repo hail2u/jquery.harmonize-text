9d29dfcc73e804cfd96e99a5b36087637c237265
Kyo Nagashima <kyo@hail2u.net>
2011-05-13 00:35:40 +0900

    check width more strictly
      * consider parent element's CSS properties
      * consider child nodes' element type and CSS properties

--- jquery.harmonize-text.js
+++ jquery.harmonize-text.js
@@ -18,9 +18,24 @@
 
         var temp = $("<div/>").css({
           "display":     "none",
-          "font-family": target.css("font-family"),
-          "font-size":   fontSize + "px"
-        }).text(target.text()).appendTo("body");
+          "font-size":   fontSize + "px",
+          "border-collapse":    target.css("border-collapse"),
+          "border-left-width":  target.css("border-left-width"),
+          "border-right-width": target.css("border-right-width"),
+          "box-sizing":         target.css("box-sizing"),
+          "font-family":        target.css("font-family"),
+          "font-style":         target.css("font-style"),
+          "font-variant":       target.css("font-variant"),
+          "font-weight":        target.css("font-weight"),
+          "letter-spacing":     target.css("letter-spacing"),
+          "padding-left":       target.css("padding-left"),
+          "padding-right":      target.css("padding-right"),
+          "text-transform":     target.css("text-transform"),
+          "white-space":        target.css("white-space"),
+          "word-break":         target.css("word-break"),
+          "word-spacing":       target.css("word-spacing"),
+          "word-wrap":          target.css("word-wrap")
+        }).append(target.contents().clone()).appendTo("body");
 
         var targetWidth = target.width();
         var tempWidth = temp.width();
